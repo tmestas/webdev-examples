f6f8ac40866f9bbb6ec275ed59e8b747197794ae
Tanner <tmestas@pdx.edu>
2022-10-26 19:10:37 -0700

    finished 01

--- javascript/exercises/01-replace.js
+++ javascript/exercises/01-replace.js
@@ -4,6 +4,13 @@ const replaceItems = (input) => {
   // for reference, see the MDN "Regular Expressions" entry:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
+  //input = input.replace(/gold/gi, 'SHINY'); // /gold/ is a Regular Expression, g and i are flags
+  
+  let re1 = /gold/gi;
+  let re2 = /wander/i;
+
+  input = input.replace(re1, 'SHINY').replace(re2, 'roam');
+
   return input;
 };
 
@@ -13,3 +20,6 @@ console.log(replaceItems('Wisdom is better than silver or GOLD.'));
 // expected: 'Wisdom is better than silver or SHINY.'
 console.log(replaceItems('Not all those who Wander are lost'));
 // expected: 'Not all those who roam are lost'
+
+
+
